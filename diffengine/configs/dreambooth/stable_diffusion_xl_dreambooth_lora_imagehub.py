f836677e1cb69d1cdb0d385c80177a6bd6f425d5
okotaku <to78314910@gmail.com>
2024-03-12 02:54:45 +0000

    Support Personalize T2I Evaluation

--- diffengine/configs/dreambooth/stable_diffusion_xl_dreambooth_lora_imagehub.py
+++ diffengine/configs/dreambooth/stable_diffusion_xl_dreambooth_lora_imagehub.py
@@ -7,8 +7,3 @@ with read_base():
     from .._base_.schedules.stable_diffusion_1k import *
 
 model.update(weight_dtype="bf16")
-
-optim_wrapper = dict(
-    type=OptimWrapper,
-    optimizer=dict(type=FusedAdam, lr=1e-5, weight_decay=1e-2),
-    clip_grad=dict(max_norm=1.0))
